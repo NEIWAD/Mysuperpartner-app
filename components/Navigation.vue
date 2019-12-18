3230c31f3019531ce2f9dbbf64842820b85fc4a1
Damien <hello@neiwad.io>
2019-12-18 12:34:49 +0100

    landing

---
+++ components/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-app-bar color="primary" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Mysuperpartner</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>A propos</v-btn>
+      <v-btn text>Contacts</v-btn>
+      <v-btn>
+        Créer un compte
+      </v-btn>
+      <v-btn>
+        S'enregistrer
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
---
+++ pages/landing.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row>
+      <v-col class="text-center">
+        <h1>{{ mainMessage }}</h1>
+        <h2>{{ releaseDate }}</h2>
+        <v-btn id="letsgoButton" class="mt-5" dark>Let's go</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  layout: 'default_noNav',
+  data() {
+    return {
+      mainMessage: 'My super partner revient vite !',
+      releaseDate: '01 / 01 / 2020'
+    }
+  }
+}
+</script>
