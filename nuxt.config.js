38bf99cc8345028badeaa17f5a94a9c5882e274b
Damien <hello@neiwad.io>
2019-12-19 16:42:14 +0100

    change landing page

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,10 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Nunito:300,400,700,900&display=swap' }
+    ]
   },
   /*
    ** Customize the progress-bar color
--- pages/landing.vue
+++ pages/landing.vue
@@ -1,22 +1,59 @@
 <template>
-  <v-container fill-height fluid>
-    <v-row>
-      <v-col class="text-center">
-        <h1>{{ mainMessage }}</h1>
-        <h2>{{ releaseDate }}</h2>
-        <v-btn id="letsgoButton" class="mt-5" dark>Let's go</v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="landingPage">
+    <div class="landingLeft">
+      <div>
+        <h1>My Super</h1>
+        <h1>Partner</h1>
+        <p>{{ subMessage }}</p>
+        <div class="d-flex">
+          <v-text-field
+            solo
+            dense
+            hide-details
+            rounded
+            placeholder="Votre adresse email"
+            class="mr-4"
+          ></v-text-field>
+          <v-btn rounded color="primary">Rester connecté !</v-btn>
+        </div>
+      </div>
+    </div>
+    <div class="landingRight"></div>
+  </div>
 </template>
 <script>
 export default {
   layout: 'default_noNav',
   data() {
     return {
-      mainMessage: 'My super partner revient vite !',
-      releaseDate: '01 / 01 / 2020'
+      subMessage:
+        'Mysuperpartner.io est la première plateforme Française de partenariats entre associations étudiantes et entreprises ! L’objectif est d’apporter de la visibilités aux associations étudiantes et de permettre aux entreprises de cibler au mieux les associations correspondants à leurs valeurs. La plateforme est actuellement en refonte et revient très bientôt, nous vous tiendrons informés.'
     }
   }
 }
 </script>
+<style lang="scss">
+.landingPage {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  font-family: 'Nunito', sans-serif;
+  div {
+    flex: 1;
+  }
+  .landingLeft {
+    display: flex;
+    align-items: center;
+    padding: 40px;
+    h1 {
+      font-size: 100px;
+      color: #368bff;
+    }
+  }
+  .landingRight {
+    display: block;
+    background-color: #face13;
+  }
+}
+</style>
